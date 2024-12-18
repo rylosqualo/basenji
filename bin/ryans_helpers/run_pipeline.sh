6ab10c6476c24675072369c1b68be5b0b9ba7c87
Ryan Keivanfar <ryankeivanfar@Ryans-MacBook-Pro-2.local>
2024-12-18 11:59:33 -0800

    paths

--- bin/ryans_helpers/run_pipeline.sh
+++ bin/ryans_helpers/run_pipeline.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
+# Set the base directory
+BASE_DIR="/clusterfs/nilah/ryank/proj/compartments/20241218_HAN_basenji"
+
 # Set the output directory
-OUTPUT_DIR="/clusterfs/nilah/ryank/proj/compartments/20241218_HAN_basenji/results"
+OUTPUT_DIR="$BASE_DIR/results"
 
 # Create output directory if it doesn't exist
 mkdir -p $OUTPUT_DIR
 
 # Launch the Nextflow pipeline
-nextflow run bin/ryans_helpers/han_pipeline.nf \
-    -c bin/ryans_helpers/nextflow.config \
+nextflow run "$BASE_DIR/bin/ryans_helpers/han_pipeline.nf" \
+    -c "$BASE_DIR/bin/ryans_helpers/nextflow.config" \
     -profile slurm \
     --output_base $OUTPUT_DIR \
     -resume
